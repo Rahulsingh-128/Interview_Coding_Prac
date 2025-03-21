5616fcef53e7edbc8ff7d8b845f1d3b2aede4ea5
Rahul <rahulbuddies007@gmail.com>
2025-03-21 12:41:49 +0530

    Promises resolved

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+// function rahul() {}
+
+// class Fifth {}
+// console.log(typeof Fifth);
+
+const handleClick = () => {
+  setTimeout(() => {
+    new Promise((resolve) => {
+      resolve("promise resolved");
+    }).then((message) => console.log(message));
+  }, 0);
+};
